3242c697b507409ffc1765fbc8acf3fb9cd65c7d
SteinArildN <sadanielse@uia.no>
2025-02-04 21:24:06 +0100

    ja fiksa analytic view og la til en ørliten filter funksjon men den skal erstattes med noe bedre

--- src/TabViews/AnalyticsView.jsx
+++ src/TabViews/AnalyticsView.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { DataContext } from "../DataContext";
 import {
   BarChart,
   Bar,
@@ -12,18 +13,22 @@ import {
   ResponsiveContainer,
 } from "recharts";
 
-import projects from "../projectsData";
-
 function AnalyticsView() {
+  const projects = useContext(DataContext);
+  console.log(projects);
+
   const [selectedRegion, setSelectedRegion] = useState("");
   const [selectedStatus, setSelectedStatus] = useState("");
   const [selectedSector, setSelectedSector] = useState("");
+
+  const [hideUndefined, setHideUndefined] = useState(false);
 
   const filteredProjects = projects.filter(
     (project) =>
       (selectedRegion === "" || project.region === selectedRegion) &&
       (selectedStatus === "" || project.status === selectedStatus) &&
-      (selectedSector === "" || project.sector === selectedSector)
+      (selectedSector === "" || project.sector === selectedSector) &&
+      (!hideUndefined || !Object.values(project).includes("Informasjon mangler"))
   );
 
   const handleResetFilters = () => {
@@ -43,6 +48,7 @@ function AnalyticsView() {
   return (
     <div className="flex flex-col gap-4 p-4 grow">
       {/* Filters container that spans full width */}
+      
       <div className="flex gap-4 mb-4 w-full">
         <select
           className="select select-bordered flex-1"
@@ -84,9 +90,18 @@ function AnalyticsView() {
           Nullstill
         </button>
       </div>
+      <label className="flex items-center gap-2">
+          <input
+            type="checkbox"
+            checked={hideUndefined}
+            onChange={(e) => setHideUndefined(e.target.checked)}
+            className="checkbox"
+          />
+          Hide Undefined Projects
+      </label>
 
       <div className="grid grid-cols-2 gap-4">
-        {["status", "region", "sector", "owner"].map((key) => (
+        {["Status", "Region", "Departement", "Prosjekteier"].map((key) => (
           <div key={key} className="card bg-base-100 shadow-xl p-4">
             <h2 className="text-lg font-bold">
               Projects by {key.charAt(0).toUpperCase() + key.slice(1)}
@@ -107,14 +122,14 @@ function AnalyticsView() {
           <ResponsiveContainer width="100%" height={300}>
             <PieChart>
               <Pie
-                data={aggregateData("status")}
+                data={aggregateData("Status")}
                 dataKey="value"
                 nameKey="name"
                 cx="50%"
                 cy="50%"
                 outerRadius={80}
               >
-                {aggregateData("status").map((entry, index) => (
+                {aggregateData("Status").map((entry, index) => (
                   <Cell
                     key={`cell-${index}`}
                     fill={["#0088FE", "#00C49F", "#FFBB28"][index % 3]}
